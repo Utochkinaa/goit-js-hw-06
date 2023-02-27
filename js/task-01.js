7077d6a7bec58c350f2c2d1de75bc65556f976e2
Utochkinaa <114943058+Utochkinaa@users.noreply.github.com>
2023-02-27 16:00:58 +0200

    added

--- js/task-01.js
+++ js/task-01.js
@@ -4,4 +4,4 @@ console.log(`Number of categories: ${categoriesList.length}`)
 categoriesList.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
     console.log(`Elements: ${item.lastElementChild.children.length}`)
-});
+}); 
